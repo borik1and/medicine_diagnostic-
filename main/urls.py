1cb406ed16ff9a14d38c47a02fd74d9eac2c4f96
Boris <borik1and@gmail.com>
2024-04-03 17:32:17 +0300

    add main, about and services

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from main.views import MainView
+from main.views import MainView, AboutView, ServicesView
 
 app_name = 'main'
 
 urlpatterns = [
     path('', MainView.as_view(), name='main'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('services/', ServicesView.as_view(), name='services'),
 ]
